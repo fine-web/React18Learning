d7464c7fbfbd1c7e207b39c191af94168472d0be
Fine Bai (Shanghai Wicresoft Co Ltd) <v-finebai@microsoft.com>
2024-07-10 22:37:34 +0800

    use props

--- src/Components/Logs/Logs.tsx
+++ src/Components/Logs/Logs.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import LogItem from './logItem'
 
-class Logs extends React.Component<{}>{
+interface IState{
+    timeArr:Array<Date>
+}
+class Logs extends React.Component<{},IState>{
     constructor(props: {}){
         super(props);
         this.state = {
             timeArr : [
                 new Date(2024,7,1),
-                new Date(2024,7,2),
-                new Date(2024,7,3),
-                new Date(2024,7,4),
+                new Date(2024,6,2),
+                new Date(2024,5,3),
+                new Date(2024,4,4),
             ]
         }
 
     }
     public render(): JSX.Element {
         return <div>
-            <LogItem></LogItem>
+            {
+                this.state.timeArr.map((time)=>{
+                    return <LogItem dateInfo = {time}></LogItem>
+                })
+            }
         </div>
     }
 }
--- src/Components/Logs/logItem.tsx
+++ src/Components/Logs/logItem.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-class LogItem extends React.Component<{},{}>{
+interface ILogDate{
+    dateInfo: Date
+}
+class LogItem extends React.Component<ILogDate,{}>{
     public render(): JSX.Element{
         return <div className="logs">
             <div className="logs-item">
                 <div className="logs-date">
-                    <div className="month">三月</div>
-                    <div className="day">22</div>
+                    <div className="month">{this.props.dateInfo.toLocaleString('zh-CN',{month:'long'})}</div>
+                    <div className="day">{this.props.dateInfo.toLocaleString('zh-CN',{day:'numeric'})}</div>
                 </div>
                 <div className="logs-item-desc">
                     <h2>React</h2>
